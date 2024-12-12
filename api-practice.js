05ed517600e320eb64668a00b45f396c8c61c0cd
Tomari <tomari7878@gmail.com>
2024-12-11 18:22:38 -0600

    finished practicing fetch and async await

--- api-practice.js
+++ api-practice.js
@@ -5,10 +5,38 @@
 //         data asynchronously over the web.
 //         fetch(url, {options})
 
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-  .then((response) => response.json())
-  .then((data) => console.log(data.id))
 
+async function fetchData() {
+  try {
+
+    const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+    if (!response.ok) {
+     throw new Error("Could not fetch resource");
+    }
+    const data = await response.json();
+    const pokemonSprite = data.sprites.front_default;
+    const imgElement = document.getElementById("pokemonSprite")
+
+    imgElement.src = pokemonSprite;
+    imgElement.style.display = "block"
+  } 
+  catch (error) {
+    console.error(error);
+  }
+}
+
+/*
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Could not fetch resource");
+    }
+    return response.json();
+  })
+  .then((data) => console.log(data.id))
   .catch((error) => console.error(error));
 
-//https://www.youtube.com/watch?v=37vxWr0WgQk
+
+*/
